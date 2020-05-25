2c891603c045675167018018c7408c0274e1ad93
PJ Hoberman <pj@twoparts.com>
2020-05-24 21:46:00 -0600

    testing around cors middleware

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+from fastapi import FastAPI
+from middleware import set_cors
 from main import app, county_list
 from tests.denver_county import data
 
@@ -10,10 +12,12 @@ def test_get_counties():
     assert response.status_code == 200
     assert response.json() == {"counties": county_list}
 
+
 def test_get_county_data():
     response = client.get('/counties/31')
     assert response.status_code == 200
     assert response.json() == data
+
 
 def test_get_county_data_county_error():
     response = client.get('/counties/RANDOM COUNTY THAT DOES NOT EXIST')
@@ -21,25 +25,19 @@ def test_get_county_data_county_error():
     assert response.json() == {"detail": "County not found in database"}
 
 
-html_test = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+def test_cors():
+    # start the sever
+    cors_app = FastAPI()
 
-    <script type="text/javascript">
-        console.log('jq');
-        $.getJSON('http://028abbd3.ngrok.io/counties', (response) => {
-            console.log(response);
-        });
+    # cors hasn't been set yet
+    assert cors_app.user_middleware == []
 
-    </script>
-</head>
-<body>
+    # set cors
+    set_cors(cors_app)
 
-</body>
-</html>
+    assert cors_app.user_middleware != []
 
-"""
+    # For now, there is only one middleware set up, but this should work for multiple
+    expected_mw_options: dict = {'allow_origins': ['*'], 'allow_credentials': True}
+    mw_options = [m.options for m in cors_app.user_middleware]
+    assert expected_mw_options in mw_options
