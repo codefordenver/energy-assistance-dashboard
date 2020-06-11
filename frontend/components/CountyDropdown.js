341bd0168485c7b8daf92adf57667ea0a11b30c8
Katie Hansen <khansen0415@gmail.com>
2020-06-11 07:53:23 -0600

    adding country dropdown and logic for api reqs

--- frontend/components/CountyDropdown.js
+++ frontend/components/CountyDropdown.js
@@ -2,14 +2,17 @@ import styles from '../styles/global.module.css'
 
 export default function CountyDropdown(props){
     const countyList = Object.entries(props.data.countyList.counties);
-
+    
     return (
-        <select className={styles.dropdown} name="county-dropdown">
-            {countyList.map((county) => {
-                return (
-                    <option key={county[0]} value={county[0]}>{county[1]}</option>
-                )
-            })}
-        </select>
+        <div className={styles['dropdown-container']}>
+            <label htmlFor="county-dropdown">Select a County: </label>
+            <select className={styles.dropdown} name="county-dropdown" onChange={(e) => props.getCountyId(e)}>
+                {countyList.map((county) => {
+                    return (
+                        <option key={county[0]} value={county[0]}>{county[1]}</option>
+                    )
+                })}
+            </select>
+        </div>
     )
 }
