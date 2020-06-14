69b83b49034085cd28aee0bca4209df34947b1c6
Katie Hansen <khansen0415@gmail.com>
2020-06-14 12:51:41 -0600

    improve design and add reusability to chart component

--- frontend/components/ParetoChart1.js
+++ frontend/components/ParetoChart1.js
@@ -7,11 +7,20 @@ import {
     HorizontalGridLines,
     LineMarkSeries 
 } from 'react-vis';
+import DiscreteColorLegend from 'react-vis/dist/legends/discrete-color-legend';
 
 const ParetoChart1 = (props) => {
+    const barKey = props.barKey
+    const lineKey = props.lineKey
+
     const rawData = props.data.selectedCountyData;
     const countyData = [];
     const householdsAssisted = [];
+
+    const LEGEND = [
+        {title: barKey, color: "#46bdc6"},
+        {title: lineKey, color: '#ff6d01'},
+    ]
 
     Object.keys(rawData).map(year => {
         countyData.push({x: year})
@@ -19,27 +28,47 @@ const ParetoChart1 = (props) => {
     })
 
     Object.entries(rawData).map((value, i) => {
-        countyData[i].y = value[1]['Households below 200% FPL'];
-        householdsAssisted[i].y = value[1]['Total Households Assisted'];
+        countyData[i].y = value[1][barKey];
+        householdsAssisted[i].y = value[1][lineKey];
     })
+
+    let yDomain = (barKey == 'Households below 200% FPL') ? [0, 125000] : [0, .4];
+    let tickValues = (barKey == 'Households below 200% FPL') ? [0, 25000, 50000, 75000, 100000, 125000] : [0, .1, .2, .3, .4];
+    let tickFormat = (barKey == '% Households below 200% FPL') ? (d) => (d * 100) + '.00%' : null;
 
     return (
         <div>
-            <XYPlot height={300} width={600} xDomain={[2020, 2008]} color="#46bdc6" stackBy="y">
+            <DiscreteColorLegend 
+                orientation="horizontal" 
+                width={410} 
+                items={LEGEND} 
+            />
+            <XYPlot 
+                height={300} 
+                width={400} 
+                xDomain={[2010, 2018]} 
+                yDomain={yDomain} 
+                color="#46bdc6"
+                className="svg-chart"
+            >
                 <VerticalGridLines />
                 <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <VerticalBarSeries 
-                    data={countyData} 
-                    style={{width: '30px' }}
+                <XAxis tickFormat={v => v.toString().replace(',', '')} />
+                <YAxis 
+                    tickValues={tickValues} 
+                    style={{ text: {transform: 'translate(0, 0)'}}} 
+                    tickFormat={tickFormat}
                 />
+                <VerticalBarSeries data={countyData}/>
                  <LineMarkSeries 
+                    strokeWidth={2}
                     data={householdsAssisted}
-                    style={{ fill: '#ff6d01'}}    
+                    stroke='#ff6d01'
+                    style={{ fill: 'none' }}
+                    markStyle={{ fill: '#ff6d01' }} 
                 />
             </XYPlot>
-        </div>
+        </div>  
     );
 }
 
