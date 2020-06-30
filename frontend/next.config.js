c6f8a2d2ca3d104ecdf0c158d1bbeb1cdacec649
Katie Hansen <khansen0415@gmail.com>
2020-06-30 17:55:22 -0600

    remove comment from config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     env: {
-        // API_ENDPOINT: process.env.NODE_ENV === ('production') 
-        //     ? process.env.NEXT_PUBLIC_API_ENDPOINT
-        //     : 'http://127.0.0.1:8000/',
         API_ENDPOINT: 'https://energy-assistance-dashboard.herokuapp.com'
     },
   }
