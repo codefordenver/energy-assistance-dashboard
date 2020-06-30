9b54e9c92546c7937c7f1d4726e8a3cf4ad99646
Josh Bernstein <joshua.bernstein@outlook.com>
2020-06-29 21:14:05 -0600

    Refactored index to be a functional component and added 404 error handling if a county did not exist

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,93 +1,137 @@
-import Head from 'next/head'
-import SummaryTable from '../components/SummaryTable';
-import CountyDropdown from '../components/CountyDropdown';
-import ParetoChart from '../components/ParetoChart';
-import HouseholdsAssisted from '../components/HouseholdsAssisted';
-import ParticipantsChart from '../components/ParticipantsChart';
-import styles from '../styles/global.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import Router, { useRouter } from "next/router";
+import Loader from "../components/Loader";
+import SummaryTable from "../components/SummaryTable";
+import CountyDropdown from "../components/CountyDropdown";
+import ParetoChart from "../components/ParetoChart";
+import HouseholdsAssisted from "../components/HouseholdsAssisted";
+import ParticipantsChart from "../components/ParticipantsChart";
+import styles from "../styles/global.module.css";
 
-const frontendUrl = 'https://energy-assistance-dashboard.herokuapp.com';
+const frontendUrl = "https://energy-assistance-dashboard.herokuapp.com";
 
-class Index extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        selectedCountyData: this.props.selectedCountyData
-    };
-  }
+function Index(props) {
+  const [selectedCountyData, setSelectedCountyData] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const countyQuery = router.query.county;
 
-  getCountyId = (e) => {
+  //req to GET specific county data
+  const getCountyData = async (id = "0") => {
+    setLoading(true);
+    const res = await fetch(`${frontendUrl}/counties/${id}`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setSelectedCountyData(data.data);
+      setError(null);
+    } else {
+      setError(res.status);
+    }
+
+    Router.push({ pathname: "/", query: { county: id } });
+    setLoading(false);
+  };
+
+  const getCountyId = (e) => {
     e.preventDefault();
     const id = e.target.value;
-    fetch(`${frontendUrl}/counties/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          selectedCountyData: data
-        })
-      })
-      .catch(err => console.log(err))
+    getCountyData(id);
+  };
+
+  if (loading) {
+    return (
+      <div className={styles.container}>
+        <Loader />
+      </div>
+    );
   }
 
-  render() {
-    const selectedCountyData = this.state.selectedCountyData.data
+  if (error) {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles["print-title"]}>
+          The selected county could not be found, please try another.
+        </h1>
+        <CountyDropdown
+          countyList={props.countyList}
+          getCountyId={getCountyId}
+          selectedCountyId={countyQuery}
+        />
+      </div>
+    );
+  }
+
+  if (!selectedCountyData) {
+    getCountyData(countyQuery);
+  } else
     return (
       <div className={styles.container}>
         <Head>
-          <title className="print-title">Energy Assistance Dashboard</title>
-          <link rel="icon" href="/favicon.png" />
+          <title className='print-title'>Energy Assistance Dashboard</title>
+          <link rel='icon' href='/favicon.png' />
         </Head>
 
         <main>
-          <div className={styles['overview']}>
+          <div className={styles["overview"]}>
             <div>
-              <h1 className={styles['print-title']}>Colorado Low Income Energy Stats</h1>
-              <CountyDropdown 
-                countyList={this.props.countyList} 
-                getCountyId={this.getCountyId}
+              <h1 className={styles["print-title"]}>
+                Colorado Low Income Energy Stats
+              </h1>
+              <CountyDropdown
+                countyList={props.countyList}
+                getCountyId={getCountyId}
+                selectedCountyId={countyQuery}
               />
-              <SummaryTable 
-                selectedCountyData={selectedCountyData} 
-              />
+
+              <SummaryTable selectedCountyData={selectedCountyData} />
             </div>
-            <img src="/energy-outreach-logo.png" alt="Energy Outreach Colorado Logo" className={styles['eoc-logo']} />
+            <img
+              src='/energy-outreach-logo.png'
+              alt='Energy Outreach Colorado Logo'
+              className={styles["eoc-logo"]}
+            />
           </div>
 
           <div>Place full stats table here!</div>
 
           <div className={styles.charts}>
             <h3>Historical Trends</h3>
-            <div className={styles['historical-trends-charts']}>
-              <ParetoChart 
-                barKey='Households below 200% FPL' 
-                lineKey='Total Households Assisted' 
+            <div className={styles["historical-trends-charts"]}>
+              <ParetoChart
+                barKey='Households below 200% FPL'
+                lineKey='Total Households Assisted'
                 selectedCountyData={selectedCountyData}
               />
-              <ParetoChart 
-                barKey='% Households below 200% FPL' 
-                lineKey='% of Households below 200% FPL Assisted' 
+              <ParetoChart
+                barKey='% Households below 200% FPL'
+                lineKey='% of Households below 200% FPL Assisted'
                 selectedCountyData={selectedCountyData}
               />
             </div>
-            <div className={styles['historical-trends-charts']}>
-              <HouseholdsAssisted 
+            <div className={styles["historical-trends-charts"]}>
+              <HouseholdsAssisted
                 title='% of Households below 200% FPL Assisted'
                 selectedCountyData={selectedCountyData}
               />
-              <ParticipantsChart
-                selectedCountyData={selectedCountyData}
-              />
+              <ParticipantsChart selectedCountyData={selectedCountyData} />
             </div>
           </div>
 
           <div className={styles.sources}>
             <h4>Sources</h4>
-            <p>American Community Survey 5-Year Estimates by the Census Bureau, Energy Outreach Colorado's households served, and CDHS LEAP households served</p>
+            <p>
+              American Community Survey 5-Year Estimates by the Census Bureau,
+              Energy Outreach Colorado's households served, and CDHS LEAP
+              households served
+            </p>
             <p>2013 LEAP data is estimated due to lack of data</p>
-            <p>Data Last Updated:  { this.state.selectedCountyData.last_updated }</p>
+            <p>Data Last Updated: {selectedCountyData.last_updated}</p>
           </div>
         </main>
-        
+
         <style jsx global>{`
           html,
           body {
@@ -96,7 +140,7 @@ class Index extends React.Component {
             font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
               Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
               sans-serif;
-              background-color: #e6e6e6;
+            background-color: #e6e6e6;
           }
           * {
             box-sizing: border-box;
@@ -128,7 +172,7 @@ class Index extends React.Component {
             overflow-y: auto;
             font-size: 12px;
           }
-         .rv-discrete-color-legend-item {
+          .rv-discrete-color-legend-item {
             color: #3a3a48;
             border-radius: 1px;
             padding: 9px 10px;
@@ -138,13 +182,15 @@ class Index extends React.Component {
             display: inline-block;
           }
 
-          .rv-discrete-color-legend-item.horizontal .rv-discrete-color-legend-item__title {
-            display:inline;
+          .rv-discrete-color-legend-item.horizontal
+            .rv-discrete-color-legend-item__title {
+            display: inline;
             padding-left: 10px;
           }
 
-          .rv-discrete-color-legend-item.vertical .rv-discrete-color-legend-item__title {
-            display:inline;
+          .rv-discrete-color-legend-item.vertical
+            .rv-discrete-color-legend-item__title {
+            display: inline;
             padding-left: 10px;
           }
           .rv-discrete-color-legend-item__color {
@@ -158,23 +204,17 @@ class Index extends React.Component {
           }
         `}</style>
       </div>
-    )
-  }
+    );
 }
 
-Index.getInitialProps = async function() {
-  //req to GET specific county data
-  const res = await fetch(`${frontendUrl}/counties/0`);
-  const data = await res.json();
-
+Index.getInitialProps = async () => {
   //req to GET all counties
   const countyRes = await fetch(`${frontendUrl}/counties`);
   const countyList = await countyRes.json();
 
   return {
-      selectedCountyData: data,
-      countyList: countyList
+    countyList: countyList,
   };
-}
+};
 
 export default Index;
