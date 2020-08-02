e38abca7e10e6f4026428c8f42fcfd1427041b22
Josh Bernstein <joshua.bernstein@outlook.com>
2020-08-02 11:49:31 -0600

    Updated dropdown so there is no delay in name change

--- frontend/components/CountyDropdown.js
+++ frontend/components/CountyDropdown.js
@@ -1,19 +1,25 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 import { getKeyByValue } from "../pages/counties";
 import styles from "../styles/global.module.css";
 
 export default function CountyDropdown(props) {
   const router = useRouter();
-  const {name} = router.query;
+  const { name } = router.query;
   const { countyList, setLoading } = props;
   const countyArray = Object.entries(countyList.counties);
-  
+
+  const [countyName, setCountyName] = useState(name)
+
   console.log(router.query);
   const getCountyData = async (id) => {
+    const selectedCountyName = countyList.counties[id];
+    
     router.push({
-      pathname: `/counties/${countyList.counties[id] }`,
+      pathname: `/counties/${selectedCountyName}`,
     });
+    setCountyName(selectedCountyName)
     setLoading(true);
   };
 
@@ -22,7 +28,7 @@ export default function CountyDropdown(props) {
     const id = e.target.value;
     getCountyData(id);
   };
-  
+
   return (
     <div className={styles["dropdown-container"]}>
       <label htmlFor='county-dropdown' className={styles["no-print"]}>
@@ -32,10 +38,16 @@ export default function CountyDropdown(props) {
       <select
         className={styles.dropdown}
         name='county-dropdown'
-        value={name ? getKeyByValue(countyList.counties, name) : 0}
+        value={countyName ? getKeyByValue(countyList.counties, countyName) : 0}
         onChange={(e) => getCountyId(e)}
       >
-        {name ? "" : <option value={0} disabled>Select a county to get started</option>}
+        {countyName ? (
+          ""
+        ) : (
+          <option value={0} disabled>
+            Select a county to get started
+          </option>
+        )}
         {countyArray.map((county) => {
           return (
             <option key={county[0]} value={county[0]}>
